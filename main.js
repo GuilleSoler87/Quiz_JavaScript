d57ff6a469eb545950bb8ec000103cb7b335791a
GuileSoler87 <guillermosoler87@gmail.com>
2023-03-13 21:51:01 +0100

    los botones funcionan

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const quiz = document.getElementById("quiz");
 const results = document.getElementById("results");
 const homeLink = document.querySelector(".home-link");
 const nextButton = document.getElementById("next-btn");
+const resultsButton = document.getElementById("result-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -11,6 +12,7 @@ const resultsHomeLink = document.querySelector(".results-home-link");
 const API_URL = "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple";
 let currentQuestionIndex;
 
+// funciones de visibilidad de navegación 
 
 function hideViews() {
   home.classList.add("hide");
@@ -22,79 +24,98 @@ function showHome() {
   hideViews();
   home.classList.remove("hide");
 }
-
+// función que trae las preguntas del API
 async function getQuestions() {
   try {
     const response = await axios.get(API_URL);
-    const questions = response.data.results[0].question;
-    const correctAnsw = response.data.results[0].correct_answer;
-    const falseAnsw = response.data.results[0].incorrect_answers;
-    const allAnswers = [...falseAnsw, correctAnsw];
-    const shuffledAnswers = shuffleArray(allAnswers);
+    const questions = response.data.results;
+    let currentQuestionIndex = 0;
+    let score = 0;
+// funcíón que llama a la siguiente pregunta
+    function nextQuestion() {
+      currentQuestionIndex++;
+      if (currentQuestionIndex < questions.length) {
+        const nextQuestionObj = questions[currentQuestionIndex];
+        const nextQuestion = nextQuestionObj.question;
+        const nextCorrectAnsw = nextQuestionObj.correct_answer;
+        const nextFalseAnsw = nextQuestionObj.incorrect_answers;
+        const nextAllAnswers = [...nextFalseAnsw, nextCorrectAnsw];
+        const nextShuffledAnswers = shuffleArray(nextAllAnswers);
 
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        showQuestion(nextQuestion, nextShuffledAnswers, nextCorrectAnsw);
+      } else {
+        // no hay más preguntas, mostrar mensaje final y guardar puntuación
+        questionElement.innerHTML = "<p>No hay más preguntas.</p>";
+        answerButtonsElement.innerHTML = "";
+        nextButton.classList.add("hide");
+
+        localStorage.setItem("score", score);
       }
-      return array;
     }
+    // función que pinta las preguntas y los botones de las posibles respuestas
+    function showQuestion(question, shuffledAnswers, correctAnsw) {
+      questionElement.innerHTML = `<p>${question}</p>`;
 
-    questionElement.innerHTML = `<p>${questions}</p>`
+      answerButtonsElement.innerHTML = "";
+      const buttons = [];
 
-    shuffledAnswers.forEach((answer) => {
-      const button = document.createElement("button");
-      button.innerHTML = answer;
-      if (answer === correctAnsw) {
-        button.dataset.correct = true;
-      }
-      
-     // button.addEventListener("click", selectAnswer);
-      answerButtonsElement.appendChild(button);
+      shuffledAnswers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer;
+        button.classList.add("btn", "btn-primary", "m-1");
+        if (answer === correctAnsw) {
+          button.dataset.correct = true;
+        }
 
-    });
-    currentQuestionIndex = 0;
-    nextButton.classList.add("hide");
+        button.addEventListener("click", () => {
+          buttons.forEach((button) => {
+            if (button.dataset.correct) {
+              button.classList.add("btn-success");
+              // si aprietas respuesta correctAnsw, sumas 1 al local storage
+              score++;
+            } else {
+              button.classList.add("btn-danger");
+            }
+            // cuando seleccionas una opción, se bloquean todas y no te permite cambiar
+            button.disabled = true;
+          });
+// cuando ya has seleccionado la respuesta y se bloquean todas a los 2 segundos pasa a la siguiente pregunta
+          setTimeout(() => {
+            nextQuestion();
+          }, 2000);
+        });
 
-    // function setStatusClass(element) {
-    //   if (element.dataset.correct) {
-    //     element.classList.add("correct");
-    //   } else {
-    //     element.classList.add("wrong");
-    //   }
-    // }
-    
-    // function setNextQuestion() {
-    //   resetState();//limpia la pregunta que había
-    //   showQuestion(questions[currentQuestionIndex]);//llamamos la función y le pasamos la pregunta actual
-    // }
-    
-    // function selectAnswer() {
-    //   Array.from(answerButtonsElement.children).forEach((button) => {
-    //     setStatusClass(button);
-    //   });
+        answerButtonsElement.appendChild(button);
+        buttons.push(button);
+      });
 
-    //   if (questions.length > currentQuestionIndex + 1) {// si quedan preguntas muestra el btn next
-    //     nextButton.classList.remove("hide");
-    //   } else {//no quedan preguntas entonces muestra el boton restart (que es el btn start)
-    //     nextButton.innerText = "Resultados";
-    //     nextButton.classList.remove("hide");
-    //   }
-    // }
-
+   
+    }
+// muestra las preguntas de forma aleatoria, gracias la función que hay debajo shuffleArray
+    showQuestion(questions[0].question, shuffleArray([...questions[0].incorrect_answers, questions[0].correct_answer]), questions[0].correct_answer);
   } catch (error) {
     console.error(error);
-    alert("No se pudieron obtener las preguntas del quiz. Inténtalo de nuevo más tarde.");
-    showHome();
   }
 }
 
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
+// funciones de vista de navegación trae la función principal de mostrar las preguntas con getQuestions
 function showQuiz() {
   hideViews();
   quiz.classList.remove("hide");
   getQuestions();
 }
 
+// traerá del local Storage la suma de todos los valores recogidos durante el cuestionario
 function showResults() {
   hideViews();
   results.classList.remove("hide");
